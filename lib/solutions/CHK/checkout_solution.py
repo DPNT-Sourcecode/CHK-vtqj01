4ec597ecee8cdc0816e066c0fc1c841c9a447f35
sbx_user1051 <sbx_user1051@169.254.132.13>
2019-09-08 16:20:49 +0000

    Sun Sep 08 16:20:49 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,35 +4,37 @@
 # skus = unicode string
 def checkout(skus):
     countOfAs, countOfBs, countOfCs, countOfDs = [0, 0, 0, 0]
-    if skus:
-        for char in skus:
-            if char == 'A':
-                countOfAs = countOfAs + 1
-            elif char == 'B':
-                countOfBs = countOfBs + 1
-            elif char == 'C':
-                countOfCs = countOfCs + 1
-            elif char == 'D':
-                countOfDs = countOfDs + 1
-            else:
-                return -1
-        totalAmountToPay = 0
-        if countOfAs:
-            itemWithOffer = int(countOfAs/3) if countOfAs >= 3 else 0
-            itemWithOutOffer = countOfAs % 3
-            totalAmountToPay = totalAmountToPay + itemWithOffer * 130 + itemWithOutOffer * 50
-        if countOfBs:
-            itemWithOffer = int(countOfBs/2) if countOfAs >= 2 else 0
-            itemWithOutOffer = countOfBs % 2
-            totalAmountToPay = totalAmountToPay + itemWithOffer * 45 + itemWithOutOffer * 30
-        if countOfCs:
-            totalAmountToPay = totalAmountToPay + countOfCs * 20
-        if countOfDs:
-            totalAmountToPay = totalAmountToPay + countOfDs * 15
+    for char in skus:
+        if char == 'A':
+            countOfAs = countOfAs + 1
+        elif char == 'B':
+            countOfBs = countOfBs + 1
+        elif char == 'C':
+            countOfCs = countOfCs + 1
+        elif char == 'D':
+            countOfDs = countOfDs + 1
+        elif char == '':
+            return 0
+        else:
+            return -1
+    totalAmountToPay = 0
+    if countOfAs:
+        itemWithOffer = int(countOfAs/3) if countOfAs >= 3 else 0
+        itemWithOutOffer = countOfAs % 3
+        totalAmountToPay = totalAmountToPay + itemWithOffer * 130 + itemWithOutOffer * 50
+    if countOfBs:
+        itemWithOffer = int(countOfBs/2) if countOfAs >= 2 else 0
+        itemWithOutOffer = countOfBs % 2
+        totalAmountToPay = totalAmountToPay + itemWithOffer * 45 + itemWithOutOffer * 30
+    if countOfCs:
+        totalAmountToPay = totalAmountToPay + countOfCs * 20
+    if countOfDs:
+        totalAmountToPay = totalAmountToPay + countOfDs * 15
 
-        return totalAmountToPay
+    return totalAmountToPay
 
     return -1
+
 
 
 
