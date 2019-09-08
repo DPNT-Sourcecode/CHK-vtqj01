101b8a159c1e54c7e7891d291004418a882134a5
sbx_user1051 <sbx_user1051@169.254.132.13>
2019-09-08 15:57:25 +0000

    Sun Sep 08 15:57:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,16 +18,27 @@ def checkout(skus):
                 return -1
         itemCount = {'A': countOfAs, 'B': countOfBs, 'C': countOfCs, 'D': countOfDs}
         totalAmountToPay = 0
-        # for item in itemCount.keys():
-        #     if item == 'A':
-        #
-        #         if itemCount[itemCount] >= 3:
-        #             pairs =
         if countOfAs:
-            setOfA = countOfAs / 3:
-            
+            itemWithOffer = countOfAs / 3
+            itemWithOutOffer = countOfAs % 3
+            totalAmountToPay = itemWithOffer * 130 + itemWithOutOffer * 50
+        if countOfBs:
+            itemWithOffer = countOfBs / 3
+            itemWithOutOffer = countOfBs % 3
+            totalAmountToPay = itemWithOffer * 130 + itemWithOutOffer * 50
+        if countOfCs:
+            itemWithOffer = countOfCs / 3
+            itemWithOutOffer = countOfCs % 3
+            totalAmountToPay = itemWithOffer * 130 + itemWithOutOffer * 50
+        if countOfDs:
+            itemWithOffer = countOfDs / 3
+            itemWithOutOffer = countOfDs % 3
+            totalAmountToPay = itemWithOffer * 130 + itemWithOutOffer * 50
+
+        return totalAmountToPay
 
     return -1
+
 
 
 
