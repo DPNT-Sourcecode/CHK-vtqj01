8aa0109235833bba657891f3baeb3b14dec554c8
sbx_user1051 <sbx_user1051@169.254.132.13>
2019-09-08 16:46:58 +0000

    Sun Sep 08 16:46:58 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,10 +5,13 @@
 def checkout(skus):
     countOfAs, countOfBs, countOfCs, countOfDs, countOfEs = [0, 0, 0, 0, 0]
     totalAmountToPay = 0
-    # priceMap = {
-    #     'A': {'Offer1':}
-    #
-    # }
+    priceMap = {
+        'A': {'SpecialOffer': 200, 'NormalOffer': 130, 'BasicPrice': 50},
+        'B': {'NormalOffer': 45, 'BasicPrice': 30},
+        'C': {'BasicPrice': 20},
+        'D': {'BasicPrice': 15},
+        'E': {'BasicPrice': 40},
+    }
     if skus:
         for char in skus:
             if char == 'A':
@@ -36,7 +39,8 @@ def checkout(skus):
                  countOfAs = itemWithOutSpecialOffer
             itemWithOffer = int(countOfAs / 3) if countOfAs >= 3 else 0
             itemWithOutOffer = countOfAs % 3
-            totalAmountToPay = totalAmountToPay + itemWithSpecialOfferOffer * 200 + itemWithOffer * 130 + itemWithOutOffer * 50
+            totalAmountToPay = totalAmountToPay + itemWithOutSpecialOffer * 200 + itemWithOffer * 130 + itemWithOutOffer * 50
+            totalAmountToPay = totalAmountToPay + itemWithOutSpecialOffer * 200 + itemWithOffer * 130 + itemWithOutOffer * 50
         if countOfBs:
             itemWithOffer = int(countOfBs / 2) if countOfBs >= 2 else 0
             itemWithOutOffer = countOfBs % 2
@@ -51,5 +55,6 @@ def checkout(skus):
             totalAmountToPay = totalAmountToPay + itemWithOffer * 30 + countOfEs * 40
 
     return totalAmountToPay if totalAmountToPay else 0
+
 
 
