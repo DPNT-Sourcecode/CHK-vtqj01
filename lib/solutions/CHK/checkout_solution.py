df8d7e63f3859877b5d606f8eae7ce8954f3f5ca
sbx_user1051 <sbx_user1051@169.254.132.13>
2019-09-08 15:51:25 +0000

    Sun Sep 08 15:51:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,18 +4,27 @@
 # skus = unicode string
 def checkout(skus):
     countOfAs, countOfBs, countOfCs, countOfDs = 0
-    for char in skus:
-        if skus == 'A':
-            countOfAs = countOfAs + 1
-        elif skus == 'B':
-            countOfBs = countOfBs + 1
-        elif skus == 'C':
-            countOfCs = countOfCs + 1
-        elif skus == 'D':
-            countOfDs = countOfDs + 1
-        else:
-            return
-    countDict = {'A': countOfAs, 'B': countOfBs, 'C': countOfCs, 'D': countOfDs}
+    if skus:
+        for char in skus:
+            if skus == 'A':
+                countOfAs = countOfAs + 1
+            elif skus == 'B':
+                countOfBs = countOfBs + 1
+            elif skus == 'C':
+                countOfCs = countOfCs + 1
+            elif skus == 'D':
+                countOfDs = countOfDs + 1
+            else:
+                return -1
+        itemCount = {'A': countOfAs, 'B': countOfBs, 'C': countOfCs, 'D': countOfDs}
+        totalAmountToPay = 0
+        for item in itemCount.keys():
+            if item == 'A':
+                if itemCount[itemCount] >
+
+    return -1
+
+
 
 
 
