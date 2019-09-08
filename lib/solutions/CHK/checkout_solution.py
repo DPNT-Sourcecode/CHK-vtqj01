405f6f329f61a0cbf39bfba470bf58f864ba2956
sbx_user1051 <sbx_user1051@169.254.132.13>
2019-09-08 15:48:25 +0000

    Sun Sep 08 15:48:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return skus
+    countOfAs, countOfBs, countOfCs, countOfDs = 0
+    for char in skus:
+        if skus == 'A':
+            countOfAs = countOfAs + 1
+        elif skus == 'B':
+            countOfBs = countOfBs + 1
+        elif skus == 'C':
+            countOfCs = countOfCs + 1
+        elif skus == 'D':
+            countOfDs = countOfDs + 1
+        else:
+            return
+    countDict = {'A': countOfAs, 'B': countOfBs, 'C': countOfCs, 'D': countOfDs}
+
+
 
